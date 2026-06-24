84115556987393cf27be6f66b92caab0553f381a
Anshul Angolkar <anshul.angolkar@coditas.com>
2026-06-24 11:53:35 +0530

    Initial Commit

---
+++ src/main/java/com/example/cohorttheplatform/CohortThePlatformApplication.java
@@ -0,0 +1,13 @@
+package com.example.cohorttheplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CohortThePlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CohortThePlatformApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/cohorttheplatform/CohortThePlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cohorttheplatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CohortThePlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
